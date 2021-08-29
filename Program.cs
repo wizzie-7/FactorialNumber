cac7a9b254656c1521735fb94d2041be29720914
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:05:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FactorialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactOfNumber FN = new FactOfNumber();
+            FN.AcceptInput();
+            FN.CalcFactorial();
+        }
+
+    }
+    public class FactOfNumber
+    {
+        int n, i;
+        int res = 1;
+        public void AcceptInput()
+        {
+            Console.WriteLine("Enter the Number :");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        public void CalcFactorial()
+        {
+            for(i=1;i<=n;i++)
+            {
+                res *= i;
+                
+            }
+            Console.Write("The factorial of "+n+ " is "+res);
+        }
+    }
+}
